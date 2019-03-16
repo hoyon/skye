15444c1b41fa3ac606c25db311ad7966ae370fb9
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-03-16 17:35:36 +0000

    use templates in recipe

--- apps/ema/lib/ema/recipe.ex
+++ apps/ema/lib/ema/recipe.ex
@@ -1,4 +1,6 @@
 defmodule Ema.Recipe do
+  alias Ema.Template
+
   def recipe do
     %{
       input: %{"user_id" => 3},
@@ -7,7 +9,7 @@ defmodule Ema.Recipe do
         {Ema.Service.Dummy, :greet, &transform1/1},
         {Ema.Service.Telegram, :send_message, &transform2/1}
       ],
-      results: []
+      results: %{}
     }
   end
 
@@ -18,21 +20,35 @@ defmodule Ema.Recipe do
 
       {:ok, result} = Ema.run_sync(elem(step, 0), elem(step, 1), input)
 
-      Map.update!(recipe, :results, fn rs -> rs ++ [result] end)
+      results = Map.merge(recipe.results, result)
+
+      Map.put(recipe, :results, results)
     end)
   end
 
   def transform1(recipe) do
-    username = Enum.at(recipe.results, 0)["username"]
-    %{"name" => username}
+    name = eval_template("Bob <{{username}}> Smith", recipe.results)
+
+    %{"name" => name}
   end
 
   def transform2(recipe) do
-    message = Enum.at(recipe.results, 1)["text"]
-    %{"text" => message}
+    text = eval_template("Message from Skye: {{text}}", recipe.results)
+    %{"text" => text}
   end
 
   def input(recipe) do
     recipe.input
+  end
+
+  defp eval_template(template, results) do
+    {:ok, ast} = Template.parse(template)
+
+    Enum.reduce(ast, "", fn item, acc ->
+      case item do
+        {:str, string} -> acc <> string
+        {:expr, expression} -> acc <> Map.get(results, expression)
+      end
+    end)
   end
 end
