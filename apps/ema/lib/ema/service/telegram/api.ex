6955183820ecc2f1f1c4b7d30048461aa2b5a60a
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-03-28 22:41:46 +0000

    ema: use binary() instead of string()

--- apps/ema/lib/ema/service/telegram/api.ex
+++ apps/ema/lib/ema/service/telegram/api.ex
@@ -5,7 +5,7 @@ defmodule Ema.Service.Telegram.Api do
   plug(Tesla.Middleware.BaseUrl, "https://api.telegram.org/bot#{Telegram.env_token()}")
   plug(Tesla.Middleware.JSON)
 
-  @callback send_message(string()) :: term()
+  @callback send_message(binary()) :: term()
   def send_message(message) do
     {:ok, response} = post("/sendMessage", %{"chat_id" => Telegram.env_chat_id(), "text" => message})
 
