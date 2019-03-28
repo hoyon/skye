dbb4e81c38489d992cff3e5e2d039cef332173bb
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-03-28 22:13:48 +0000

    trucy: Add tachyons

---
+++ trucy/index.js
@@ -0,0 +1,2 @@
+require('./src/Index.bs.js');
+require('./index.css');
--- trucy/webpack.config.js
+++ trucy/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const outputDir = path.join(__dirname, 'build');
 
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './src/Index.bs.js',
+  entry: './index.js',
   mode: isProd ? 'production' : 'development',
   output: {
     path: outputDir,
@@ -17,6 +18,17 @@ module.exports = {
       inject: true
     })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
   devServer: {
     compress: true,
     contentBase: outputDir,
